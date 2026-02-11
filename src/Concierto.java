cf7c2e7138e8732d4a51e60c1bb919bd5f61b288
Daaniel-Sans <danpavtel@gmail.com>
2026-02-11 18:50:25 +0100

    Add attributes to Concierto class

--- src/Concierto.java
+++ src/Concierto.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Concierto {
+
+    // 1. Atributos
+    private String artista;
+    private String ciudad;
+    private double precioBase;
+    private int aforoMaximo;
+    private ArrayList<Entrada> entradasVendidas;
+    private boolean activo;
+
+
 }
