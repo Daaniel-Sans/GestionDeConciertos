48383a3aff55e1c94dcfb0e697449febd06d2c91
Daaniel-Sans <danpavtel@gmail.com>
2026-02-11 19:11:32 +0100

    Add attributes and constructors to Usuario class

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Usuario {
+
+    //Atributos
+
+    private String nombre;
+    private int edad;
+    private ArrayList<Entrada> entradasCompradas;
+    private HashSet<Concierto> conciertosAsistidos;
+    private HashMap<Concierto, Integer> valoraciones;
+
+    //Constructor vacío
+
+    public Usuario() {
+    }
+
+    //Constructor completo
+
+    public Usuario(String nombre, int edad, ArrayList<Entrada> entradasCompradas, HashSet<Concierto> conciertosAsistidos, HashMap<Concierto, Integer> valoraciones) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.entradasCompradas = entradasCompradas;
+        this.conciertosAsistidos = conciertosAsistidos;
+        this.valoraciones = valoraciones;
+    }
 }
