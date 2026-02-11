f8e9d29a7c96df34423544686cacec20c1681f32
Daaniel-Sans <danpavtel@gmail.com>
2026-02-11 20:06:28 +0100

    Create 3 concerts

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+
+        // 1. Crear 3 conciertos
+        Concierto c1 = new Concierto("Perrate de Utrera", "Utrera", 40.0, 80, new ArrayList<>(), true);
+        Concierto c2 = new Concierto("David Palomar", "Cádiz", 35.0, 120, new ArrayList<>(), true);
+        Concierto c3 = new Concierto("Estrella Morente", "Granada", 55.0, 150, new ArrayList<>(), true);
 
     }
 }
