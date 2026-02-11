8c22a9ee069a9d720e94fcf170a25fb5a53d6aac
Daaniel-Sans <danpavtel@gmail.com>
2026-02-11 18:04:47 +0100

    Add constructors to Entrada class

--- src/Entrada.java
+++ src/Entrada.java
@@ -1,2 +1,26 @@
 public class Entrada {
+
+    //Atributos concierto (de tipo Concierto)
+    public enum TipoEntrada { PISTA, GRADA, VIP }
+
+    /*Atributo tipo (enumerado que puede ser Pista, Grada o VIP).
+    Yo llamaré tipoEntrada al tipo para evitar redundancias.*/
+
+    private Concierto concierto;
+    private TipoEntrada tipo;
+
+
+
+    //Constructor vacío
+    public Entrada() {
+    }
+
+    //Constructor completo
+    public Entrada(Concierto concierto, TipoEntrada tipo) {
+        this.concierto = concierto;
+        this.tipo = tipo;
+    }
+
+
+
 }
