0fe6a0de31e5dedfc76e9b3bdd8a5cb3fb2a6013
Daaniel-Sans <danpavtel@gmail.com>
2026-02-11 19:13:16 +0100

    Add getters,setters,equals and hashCode to Usuario class

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Objects;
 
 public class Usuario {
 
@@ -25,5 +26,61 @@ public class Usuario {
         this.entradasCompradas = entradasCompradas;
         this.conciertosAsistidos = conciertosAsistidos;
         this.valoraciones = valoraciones;
+    }
+
+    //Getters y setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public ArrayList<Entrada> getEntradasCompradas() {
+        return entradasCompradas;
+    }
+
+    public void setEntradasCompradas(ArrayList<Entrada> entradasCompradas) {
+        this.entradasCompradas = entradasCompradas;
+    }
+
+    public HashSet<Concierto> getConciertosAsistidos() {
+        return conciertosAsistidos;
+    }
+
+    public void setConciertosAsistidos(HashSet<Concierto> conciertosAsistidos) {
+        this.conciertosAsistidos = conciertosAsistidos;
+    }
+
+    public HashMap<Concierto, Integer> getValoraciones() {
+        return valoraciones;
+    }
+
+    public void setValoraciones(HashMap<Concierto, Integer> valoraciones) {
+        this.valoraciones = valoraciones;
+    }
+
+    //Equals y hashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return edad == usuario.edad && Objects.equals(nombre, usuario.nombre) && Objects.equals(entradasCompradas, usuario.entradasCompradas) && Objects.equals(conciertosAsistidos, usuario.conciertosAsistidos) && Objects.equals(valoraciones, usuario.valoraciones);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, edad, entradasCompradas, conciertosAsistidos, valoraciones);
     }
 }
